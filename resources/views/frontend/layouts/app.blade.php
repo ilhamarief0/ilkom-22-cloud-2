62ac4ab68db4653ac89e5ea24852bba51c3cc2f7
pitra16 <ependipitra@gmail.com>
2025-06-30 16:00:30 +0800

    update app.php

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,17 +1,19 @@
-<!doctype html>
-<html lang="en">
+<!doctype html> <!-- Menentukan bahwa ini adalah dokumen HTML5 -->
+<html lang="en"> <!-- Tag pembuka HTML dengan atribut bahasa -->
 
 <head>
+    <!-- Metadata dasar dan pengaturan halaman -->
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="description" content="A modern and responsive electronics store template" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
+    <!-- Judul dan favicon -->
     <title>Radios - Electronics Store WooCommerce Template</title>
     <link rel="shortcut icon" href="assets/img/favicon.png" type="images/x-icon" />
 
-    <!-- css include -->
+    <!-- Memuat file-file CSS eksternal yang digunakan untuk styling -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/fontawesome.css" />
     <link rel="stylesheet" href="assets/css/animate.css" />
@@ -24,8 +26,9 @@
 </head>
 
 <body>
-    <div class="body_wrap">
-        <!-- Preloader -->
+    <div class="body_wrap"> <!-- Pembungkus seluruh isi halaman -->
+
+        <!-- Animasi loading saat halaman dibuka -->
         <div class="preloader_part">
             <div class="spinner">
                 <div class="dot1"></div>
@@ -33,19 +36,20 @@
             </div>
         </div>
 
-        <!-- Back to top -->
+        <!-- Tombol kembali ke atas dengan animasi lingkaran -->
         <div class="progress-wrap">
             <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
                 <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
             </svg>
         </div>
 
+        <!-- Menyisipkan bagian header, sidebar, konten, dan footer menggunakan Blade -->
         @include('frontend.layouts.header')
         @include('frontend.layouts.sidebar')
         @yield('content')
         @include('frontend.layouts.footer')
 
-        <!-- Cookie Consent -->
+        <!-- Area pemberitahuan penggunaan cookie -->
         <div class="cookies-area">
             <p>This website uses cookies to improve your experience. By using this website, you agree to our <a href="#">Data Protection Policy</a>.</p>
             <a href="#" class="read-more">Read more</a>
@@ -55,9 +59,10 @@
         </div>
     </div>
 
+    <!-- Tempat menyisipkan script tambahan melalui Blade -->
     @stack('script')
 
-    <!-- jquery include -->
+    <!-- Memuat file-file JavaScript eksternal untuk interaksi dan fungsionalitas halaman -->
     <script src="assets/js/jquery-3.5.1.min.js"></script>
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/slick.js"></script>
@@ -74,4 +79,4 @@
     <script src="assets/js/main.js"></script>
 </body>
 
-</html>
+</html> <!-- Akhir dokumen HTML -->
