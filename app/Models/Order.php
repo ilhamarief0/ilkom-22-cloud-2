e04aad7c419ab569b1a6022c6fd2f1ab956baac3
Jawir0 <Muh.ibnumagelang01@gmail.com>
2025-07-03 03:24:35 +0800

    add coment

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,7 +8,7 @@ class Order extends Model
 {
     protected $fillable = [
         'order_id',// ID unik untuk pesanan
-        'user_id',
+        'user_id',// ID user yang membuat pesanan
         'total_price',
         'status'
     ];
