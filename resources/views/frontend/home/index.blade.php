db97b26b62b6ce7b8b8ef0f7ce6a56b07edac37b
Sann094 <bintangalnur88@gmail.com>
2025-07-02 22:29:30 +0800

    mengubah harga Acer SB220Q bi 21.5 Inches Full HD

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -501,7 +501,7 @@
                                                 <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                             <h2 class="product__title"><a href="shop-single.html">Acer SB220Q bi 21.5 Inches Full HD </a></h2>
-                                            <h4 class="product__price"><span class="new">$30.52</span><span class="old">$28.52</span></h4>
+                                            <h4 class="product__price"><span class="new">$55.52</span><span class="old">$28.52</span></h4>
                                         </div>
                                         <ul class="product__action">
                                             <li><a href="#!"><i class="far fa-compress-alt"></i></a></li>
