a2bb628fe552b907e412686a2517fa49122cb9d7
FaizalMuhammad <22muh.izal@gmail.com>
2025-07-02 19:29:58 +0700

    work optimization on index dashboard

--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -223,74 +223,82 @@
 										</div>
 										<!--end::Mixed Widget 1-->
 									</div>
-									<div class="col-lg-6 col-xxl-4">
-										<!--begin::List Widget 9-->
-										<div class="card card-custom card-stretch gutter-b">
-											<!--begin::Header-->
-											<div class="card-header align-items-center border-0 mt-4">
-												<h3 class="card-title align-items-start flex-column">
-													<span class="font-weight-bolder text-dark">My Activity</span>
-													<span class="text-muted mt-3 font-weight-bold font-size-sm">890,344 Sales</span>
-												</h3>
-												<div class="card-toolbar">
-													<div class="dropdown dropdown-inline">
-														<a href="#" class="btn btn-clean btn-hover-light-primary btn-sm btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-															<i class="ki ki-bold-more-hor"></i>
-														</a>
-														<div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
-															<!--begin::Navigation-->
-															<ul class="navi navi-hover">
-																<li class="navi-header font-weight-bold py-4">
-																	<span class="font-size-lg">Choose Label:</span>
-																	<i class="flaticon2-information icon-md text-muted" data-toggle="tooltip" data-placement="right" title="Click to learn more..."></i>
-																</li>
-																<li class="navi-separator mb-3 opacity-70"></li>
-																<li class="navi-item">
-																	<a href="#" class="navi-link">
-																		<span class="navi-text">
-																			<span class="label label-xl label-inline label-light-success">Customer</span>
-																		</span>
-																	</a>
-																</li>
-																<li class="navi-item">
-																	<a href="#" class="navi-link">
-																		<span class="navi-text">
-																			<span class="label label-xl label-inline label-light-danger">Partner</span>
-																		</span>
-																	</a>
-																</li>
-																<li class="navi-item">
-																	<a href="#" class="navi-link">
-																		<span class="navi-text">
-																			<span class="label label-xl label-inline label-light-warning">Suplier</span>
-																		</span>
-																	</a>
-																</li>
-																<li class="navi-item">
-																	<a href="#" class="navi-link">
-																		<span class="navi-text">
-																			<span class="label label-xl label-inline label-light-primary">Member</span>
-																		</span>
-																	</a>
-																</li>
-																<li class="navi-item">
-																	<a href="#" class="navi-link">
-																		<span class="navi-text">
-																			<span class="label label-xl label-inline label-light-dark">Staff</span>
-																		</span>
-																	</a>
-																</li>
-																<li class="navi-separator mt-3 opacity-70"></li>
-																<li class="navi-footer py-4">
-																	<a class="btn btn-clean font-weight-bold btn-sm" href="#">
-																	<i class="ki ki-plus icon-sm"></i>Add new</a>
-																</li>
-															</ul>
-															<!--end::Navigation-->
+								<div class="col-lg-6 col-xxl-4">
+    <!--begin::List Widget 9-->
+												<div class="card card-custom card-stretch gutter-b">
+													<!--begin::Header-->
+													<div class="card-header align-items-center border-0 mt-4">
+														<h3 class="card-title align-items-start flex-column">
+															<span class="font-weight-bolder text-dark">My Activity</span>
+															<span class="text-muted mt-3 font-weight-bold font-size-sm">890,344 Sales</span>
+														</h3>
+														<div class="card-toolbar">
+															<div class="dropdown dropdown-inline">
+																<a href="#" class="btn btn-clean btn-hover-light-primary btn-sm btn-icon" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="More Options">
+																	<i class="ki ki-bold-more-hor"></i>
+																</a>
+																<div class="dropdown-menu dropdown-menu-md dropdown-menu-right">
+																	<!--begin::Navigation-->
+																	<ul class="navi navi-hover">
+																		<li class="navi-header font-weight-bold py-4">
+																			<span class="font-size-lg">Choose Label:</span>
+																			<i class="flaticon2-information icon-md text-muted" data-toggle="tooltip" data-placement="right" title="Click to learn more..."></i>
+																		</li>
+																		<li class="navi-separator mb-3 opacity-70"></li>
+
+																		<li class="navi-item">
+																			<a href="#" class="navi-link">
+																				<span class="navi-text">
+																					<span class="label label-xl label-inline label-light-success">Customer</span>
+																				</span>
+																			</a>
+																		</li>
+																		<li class="navi-item">
+																			<a href="#" class="navi-link">
+																				<span class="navi-text">
+																					<span class="label label-xl label-inline label-light-danger">Partner</span>
+																				</span>
+																			</a>
+																		</li>
+																		<li class="navi-item">
+																			<a href="#" class="navi-link">
+																				<span class="navi-text">
+																					<span class="label label-xl label-inline label-light-warning">Supplier</span>
+																				</span>
+																			</a>
+																		</li>
+																		<li class="navi-item">
+																			<a href="#" class="navi-link">
+																				<span class="navi-text">
+																					<span class="label label-xl label-inline label-light-primary">Member</span>
+																				</span>
+																			</a>
+																		</li>
+																		<li class="navi-item">
+																			<a href="#" class="navi-link">
+																				<span class="navi-text">
+																					<span class="label label-xl label-inline label-light-dark">Staff</span>
+																				</span>
+																			</a>
+																		</li>
+
+																		<li class="navi-separator mt-3 opacity-70"></li>
+																		<li class="navi-footer py-4">
+																			<a class="btn btn-clean font-weight-bold btn-sm" href="#">
+																				<i class="ki ki-plus icon-sm"></i>Add new
+																			</a>
+																		</li>
+																	</ul>
+																	<!--end::Navigation-->
+																</div>
+															</div>
 														</div>
 													</div>
+													<!--end::Header-->
 												</div>
+												<!--end::List Widget 9-->
 											</div>
+
 											<!--end::Header-->
 											<!--begin::Body-->
 											<div class="card-body pt-4">
