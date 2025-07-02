4a805896bebadae5fc6f97307f2b6c40f9bad841
FaizalMuhammad <22muh.izal@gmail.com>
2025-07-02 19:13:58 +0700

    backend footer optimization

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,21 +1,21 @@
 <!--begin::Footer-->
-<div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
-    <!--begin::Container-->
+<footer class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
     <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
-        <!--begin::Copyright-->
+        
+        <!-- Copyright -->
         <div class="text-dark order-2 order-md-1">
-            <span class="text-muted font-weight-bold mr-2">2021©</span>
-            <a href="http://keenthemes.com/metronic" target="_blank" class="text-dark-75 text-hover-primary">Keenthemes</a>
+            <span class="text-muted font-weight-bold me-2">© 2021</span>
+            <a href="https://keenthemes.com/metronic" target="_blank" rel="noopener" class="text-dark-75 text-hover-primary">Keenthemes</a>
         </div>
-        <!--end::Copyright-->
-        <!--begin::Nav-->
-        <div class="nav nav-dark">
-            <a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pl-0 pr-5">About</a>
-            <a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pl-0 pr-5">Team</a>
-            <a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pl-0 pr-0">Contact</a>
-        </div>
-        <!--end::Nav-->
+
+        <!-- Navigation -->
+        <nav class="nav nav-dark order-1 order-md-2">
+            <a href="https://keenthemes.com/metronic" target="_blank" rel="noopener" class="nav-link px-3">About</a>
+            <a href="https://keenthemes.com/metronic" target="_blank" rel="noopener" class="nav-link px-3">Team</a>
+            <a href="https://keenthemes.com/metronic" target="_blank" rel="noopener" class="nav-link px-3">Contact</a>
+        </nav>
+
     </div>
-    <!--end::Container-->
-</div>
+</footer>
+
 <!--end::Footer-->
