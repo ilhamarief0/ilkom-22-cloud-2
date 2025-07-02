ee193468e96a493b27abffc94bcb1c0d7dc9244b
Sann094 <bintangalnur88@gmail.com>
2025-07-02 22:38:22 +0800

    mengubah Chromebook Flip Laptop, 14″ Full HD menjadi Chromebook Flip Laptop,  Full HD

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -685,7 +685,7 @@
                                             <div class="product__progress progress">
                                                 <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
-                                            <h2 class="product__title"><a href="shop-single.html">Chromebook Flip Laptop, 14″ Full HD</a></h2>
+                                            <h2 class="product__title"><a href="shop-single.html">Chromebook Flip Laptop, Full HD</a></h2>
                                             <h4 class="product__price"><span class="new">$30.52</span><span class="old">$28.52</span></h4>
                                         </div>
                                         <ul class="product__action">
