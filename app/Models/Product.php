1219bf2d831cbc12e6b10c7db17a356819cf3428
Yuzlih <yuzlihpie05@gmail.com>
2025-07-03 20:36:07 +0800

    tambahin model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'product_category_id',
+        'product_name',
+        'product_desc',
+        'product_price',
+        'product_images',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Product_category::class, 'product_category_id');
+    }
 }
