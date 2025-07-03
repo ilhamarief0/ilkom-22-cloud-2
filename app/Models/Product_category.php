fa76973befda944954390acc4f34f105f60f85bb
Yuzlih <yuzlihpie05@gmail.com>
2025-07-03 20:53:37 +0800

    perbaiki isi product category

--- app/Models/Product_category.php
+++ app/Models/Product_category.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product_category extends Model
+class Product_Category extends Model
 {
-    //
+    protected $table = 'product_categories';
+
+    protected $fillable = [
+        'name', // tambahkan jika field ini ada di tabel
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'product_category_id');
+    }
 }
